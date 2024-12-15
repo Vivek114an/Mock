5ce8f31c3ac44d09b2f7cdb3aca667c7fe0a2a2a
Vivek114an <vs865318@gmail.com>
2024-12-15 13:09:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let questions = [];
+let currentQuestionIndex = 0;
+let timerInterval;
+let remainingTime = 2400; // 40 minutes in seconds
 
 function showPage(pageNum) {
     document.getElementById('page1').classList.add('hidden');
@@ -10,6 +13,7 @@ function showPage(pageNum) {
     } else if (pageNum === 2) {
         document.getElementById('page2').classList.remove('hidden');
         loadMockTest();
+        startTimer();
     } else if (pageNum === 3) {
         document.getElementById('page3').classList.remove('hidden');
         showResults();
@@ -33,51 +37,95 @@ function saveQuestion() {
 
     questions.push(question);
     alert("Question saved!");
+    updateQuestionsList();
+}
+
+function updateQuestionsList() {
+    let list = document.getElementById('questionsList');
+    list.innerHTML = '';
+    questions.forEach((q, i) => {
+        let li = document.createElement('li');
+        li.textContent = `Q${i + 1}: ${q.text}`;
+        list.appendChild(li);
+    });
 }
 
 function loadMockTest() {
     let testContainer = document.getElementById('testQuestions');
     testContainer.innerHTML = '';
 
-    questions.forEach((question, index) => {
-        let questionDiv = document.createElement('div');
-        
-        let questionContent = document.createElement('div');
-        if (question.image) {
-            let img = document.createElement('img');
-            img.src = question.image;
-            questionContent.appendChild(img);
-        } else {
-            questionContent.textContent = question.text;
-        }
-        
-        questionDiv.appendChild(questionContent);
-        
-        let optionsDiv = document.createElement('div');
-        question.options.forEach((option, i) => {
-            let label = document.createElement('label');
-            let input = document.createElement('input');
-            input.type = 'radio';
-            input.name = 'question' + index;
-            input.value = option;
-            label.appendChild(input);
-            label.append(option);
-            optionsDiv.appendChild(label);
-        });
-
-        questionDiv.appendChild(optionsDiv);
-        testContainer.appendChild(questionDiv);
+    let question = questions[currentQuestionIndex];
+    let questionDiv = document.createElement('div');
+    
+    let questionContent = document.createElement('div');
+    if (question.image) {
+        let img = document.createElement('img');
+        img.src = question.image;
+        questionContent.appendChild(img);
+    } else {
+        questionContent.textContent = question.text;
+    }
+    questionDiv.appendChild(questionContent);
+    
+    let optionsDiv = document.createElement('div');
+    question.options.forEach((option, i) => {
+        let label = document.createElement('label');
+        let input = document.createElement('input');
+        input.type = 'radio';
+        input.name = 'question' + currentQuestionIndex;
+        input.value = option;
+        label.appendChild(input);
+        label.append(option);
+        optionsDiv.appendChild(label);
     });
+
+    questionDiv.appendChild(optionsDiv);
+    testContainer.appendChild(questionDiv);
+}
+
+function startTimer() {
+    timerInterval = setInterval(() => {
+        if (remainingTime <= 0) {
+            clearInterval(timerInterval);
+            submitTest();
+        } else {
+            remainingTime--;
+            updateTimerDisplay();
+        }
+    }, 1000);
+}
+
+function updateTimerDisplay() {
+    let minutes = Math.floor(remainingTime / 60);
+    let seconds = remainingTime % 60;
+    document.getElementById('timer').textContent = `Time Left: ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
 
 function submitTest() {
+    clearInterval(timerInterval);
+
     questions.forEach((question, index) => {
         let selectedOption = document.querySelector(`input[name="question${index}"]:checked`);
         if (selectedOption) {
             question.answer = selectedOption.value;
         }
     });
+    
     showPage(3);
+}
+
+function nextQuestion() {
+    if (currentQuestionIndex < questions.length - 1) {
+        currentQuestionIndex++;
+        loadMockTest();
+    }
+}
+
+function previousQuestion() {
+    if (currentQuestionIndex > 0) {
+        currentQuestionIndex--;
+        loadMockTest();
+    }
 }
 
 function showResults() {
